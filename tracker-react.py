a979cad0e98f71df1a6b9186b42906ffcd3d16d1
Ashwin Mahesh <ashwinmahesh@gmail.com>
2018-12-26 23:42:34 -0500

    optimized dictionaey stuff
    got rid of getMemberFromID

--- tracker-react.py
+++ tracker-react.py
@@ -1,4 +1,4 @@
-# Work with Python 3.5
+# Works with Python 3.5.4
 import discord
 import json
 from collections import OrderedDict
@@ -15,55 +15,32 @@ async def get_logs(channel):
     print(len(msg_list), " messages read")
     return msg_list
 
-async def getNameFromID(id, list):
-    for member in list:
-        if (member.id == id):
-            return member
-
 async def count_reacts(emoji, msg_list):
     new_dict = {}
     for m in msg_list:
          if (not m.author.bot):
              for react in m.reactions:
                  if(react.emoji == emoji):
-                    if (m.author.id in new_dict):
-                        new_dict[m.author.id] = new_dict[m.author.id] + react.count
+                    if (m.author.id in new_dict.keys()):
+                        new_dict[m.author.id]["count"] = new_dict[m.author.id]["count"] + react.count
                     else:
-                        new_dict[m.author.id] = react.count
+                        new_dict[m.author.id] = { 
+                            "member" : m.author,
+                            "count" : react.count
+                        }
+
     return new_dict
 
 async def get_leaders(emoji, member_dict, channel, member_list):
     mem_count = 1
     s = "These are the top 10 users with " + emoji + " reacts on #" + str(channel) + '\n'
     for key, value in sorted(member_dict.items(), key=lambda kv: kv[1], reverse=True):
-        name = await getNameFromID(key, member_list)
-        s = s + str(mem_count) + ". " + str(name) + " : " + str(value) + '\n'
-        mem_count = mem_count+1
+        s = s + str(mem_count) + ". " + str(value["member"]) + " : " + str(value["count"]) + '\n'
+        mem_count = mem_count + 1
         if (mem_count>=11):
             break
     
     return s
-
-async def win(author, channel, member_list, msg_list):
-    member_dict = await count_reacts("🇼", msg_list)
-    print(len(member_dict))
-    stats_msg = await get_leaders("🇼", member_dict, channel, member_list)
-
-    return stats_msg
-    
-async def joy(author, channel, member_list, msg_list):
-    member_dict = await count_reacts("😂", msg_list)
-
-    stats_msg = await get_leaders("😂", member_dict, channel, member_list)
-
-    return stats_msg
-
-async def loss(author, channel, member_list, msg_list):
-    member_dict = await count_reacts("🇱", msg_list)
-
-    stats_msg = await get_leaders("🇱", member_dict, channel, member_list)
-
-    return stats_msg
 
 async def hello(author):
     return "Hello " + author.mention
@@ -76,7 +53,7 @@ async def help(author): # Modify based on commands added
 
     Usage - !stats <command>
 
-    valid commands - win, loss, joy, help, hello
+    valid commands - win, loss, joy, syringe, pens(disappointed), help, hello
     """
 
 @client.event
@@ -89,15 +66,25 @@ async def on_message(message):
         print("calling ", val)
 
         if not(message.author.id == '160157662204526602' or message.author.id== '424975538721914900' or val == 'hello' or val == 'help'):
-            return "You Are Not Authorized To Use This Feature. Pay $100 to Fcord to continue."
+            return "You Are Not Authorized To Use This Command. Pay $100 to Fcord to continue."
         
         if(val == 'help' or val =='hello'):
             await client.send_message(message.channel, await globals()[val](message.author))  
         else:
             member_list= message.channel.server.members
             msg_list = await get_logs(message.channel)
-           
-            msg = await globals()[val](message.author, message.channel, member_list, msg_list) #call corresponding function
+            
+            emoji_dict = {
+                "win" : "🇼",
+                "loss" : "🇱",
+                "syringe" : "💉" ,
+                "pens" : "😞",
+                "joy" : "😂"
+            }
+
+            member_dict = await count_reacts(emoji_dict[val], msg_list)
+
+            msg = await get_leaders(emoji_dict[val], member_dict, message.channel, member_list)
         
             if not(msg is None):
                 await client.send_message(message.channel, msg)
